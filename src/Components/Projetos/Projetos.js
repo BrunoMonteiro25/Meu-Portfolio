f335ed68ba554c7b5f814cd37d60415fe36209bf
BrunoMonteiro25 <bruno.monteiro.silva25@gmail.com>
2022-09-13 01:03:48 -0300

    Projetos

--- src/Components/Projetos/Projetos.js
+++ src/Components/Projetos/Projetos.js
@@ -1,8 +1,46 @@
 import React from 'react'
-import { MeusProjetos } from './ProjetosStyled'
+import {
+  Botoes,
+  Demo,
+  Imagem,
+  MeusProjetos,
+  Projeto,
+  Repositorio,
+  SobreContainer,
+  TecnologiasUsadas,
+  Titulo,
+  TituloProjeto,
+} from './ProjetosStyled'
+import Dogs from '../../../src/Assets/dogs.png'
+import { FaExternalLinkAlt, FaGithub } from 'react-icons/fa'
 
 const Projetos = () => {
-  return <MeusProjetos>Meus Projetos</MeusProjetos>
+  return (
+    <MeusProjetos>
+      <TituloProjeto>Meus Projetos</TituloProjeto>
+      <Projeto>
+        <Imagem src={Dogs} alt="Dogs" />
+        <SobreContainer>
+          <Titulo>Projeto Dogs</Titulo>
+          Uma rede social tipo instagram, mas para cachorros.
+          <TecnologiasUsadas>
+            <span>Tecnologias usadas: </span>HTML5, CSS3 (FlexBox e CSS Grid),
+            JavaScript, ReactJS.
+          </TecnologiasUsadas>
+          <Botoes>
+            <Demo>
+              <FaExternalLinkAlt />
+              Demo
+            </Demo>
+            <Repositorio>
+              <FaGithub />
+              Repositório
+            </Repositorio>
+          </Botoes>
+        </SobreContainer>
+      </Projeto>
+    </MeusProjetos>
+  )
 }
 
 export default Projetos
--- src/Components/Projetos/ProjetosStyled.js
+++ src/Components/Projetos/ProjetosStyled.js
@@ -2,6 +2,94 @@ import styled from 'styled-components'
 
 export const MeusProjetos = styled.div`
   border: 1px solid #fff;
+  color: #fff;
+  padding: 1rem;
   height: max-content;
-  grid-row: span -1/1;
+  background: rgba(52, 48, 51, 0.39);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(6.6px);
+  -webkit-backdrop-filter: blur(6.6px);
+  border: 2px solid #dcb6e1;
+`
+
+export const TituloProjeto = styled.p`
+  font-weight: 600;
+  font-size: 1.3rem;
+`
+
+export const Projeto = styled.div`
+  margin-top: 1rem;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  gap: 1rem;
+`
+
+export const Imagem = styled.img`
+  max-width: 200px;
+  height: 150px;
+  background-size: cover;
+  border-radius: 1rem;
+`
+
+export const SobreContainer = styled.div`
+  font-size: 0.8rem;
+  display: grid;
+  gap: 0.3rem;
+`
+
+export const Titulo = styled.p`
+  font-size: 1rem;
+  font-weight: 400;
+  color: #fff;
+`
+
+export const TecnologiasUsadas = styled.p`
+  font-weight: 400;
+  font-size: 0.8rem;
+  margin-top: 1rem;
+  line-height: 1.4;
+
+  span {
+    font-size: 1rem;
+  }
+`
+
+export const Botoes = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: end;
+  margin-top: 1rem;
+`
+
+export const Demo = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 0.4rem;
+  font-size: 0.9rem;
+  padding: 0.5rem 1rem;
+  background: #26569d;
+  border-radius: 4px;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: #fff;
+  font-weight: bold;
+  max-width: max-content;
+  border: none;
+  cursor: pointer;
+  transition: all 0.3s;
+
+  :hover {
+    background: #0975b8;
+  }
+`
+
+export const Repositorio = styled(Demo)`
+  background: #000;
+  color: #fff;
+  margin-left: 1rem;
+
+  :hover {
+    background: #222;
+  }
 `
